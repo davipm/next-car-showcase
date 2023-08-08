f13de02071e2c9b45124c859dcc76486ed94ec05
davi_mendoca <davi.p.m94@gmail.com>
2023-08-08 15:19:08 -0300

    adding test

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -18,12 +18,13 @@ export default function CustomButton({
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
+      data-testid="custom-button"
     >
       <span className={`flex-1 ${textStyles}`}>{title}</span>
       {rightIcon && (
         <div className="relative w-6 h-6">
           <Image
-            src={rightIcon}
+            src={`/${rightIcon}`}
             alt="arrow_left"
             fill
             className="object-contain"
---
+++ src/components/__tests__/CustomButton.test.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import CustomButton from "@/components/CustomButton";
+import { CustomButtonProps } from "@/@types";
+
+// Mock the props for testing
+const mockProps: CustomButtonProps = {
+  handleClick: jest.fn(),
+  containerStyles: "test-container",
+  textStyles: "test-text",
+  btnType: "button",
+  title: "Test Button",
+  rightIcon: "test-icon.png",
+  isDisabled: false,
+};
+
+describe("CustomButton", () => {
+  it("renders correctly with provided props", () => {
+    const { getByText, getByAltText } = render(<CustomButton {...mockProps} />);
+
+    const buttonElement = getByText(mockProps.title);
+    const iconElement = getByAltText("arrow_left");
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(iconElement).toBeInTheDocument();
+  });
+
+  it("calls the handleClick function when clicked", () => {
+    const { getByText } = render(<CustomButton {...mockProps} />);
+
+    const buttonElement = getByText(mockProps.title);
+    fireEvent.click(buttonElement);
+
+    expect(mockProps.handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("renders with the correct containerStyles and textStyles classes", () => {
+    const { containerStyles, textStyles } = mockProps;
+    const { getByTestId } = render(<CustomButton {...mockProps} />);
+
+    const buttonElement = getByTestId("custom-button");
+    expect(buttonElement).toHaveClass("custom-btn");
+    // @ts-ignore
+    expect(buttonElement).toHaveClass(containerStyles);
+    // @ts-ignore
+    expect(buttonElement.querySelector(".flex-1")).toHaveClass(textStyles);
+  });
+
+  it("renders as disabled when isDisabled prop is true", () => {
+    const { getByTestId } = render(
+      <CustomButton {...mockProps} isDisabled={true} />,
+    );
+
+    const buttonElement = getByTestId("custom-button");
+    expect(buttonElement).toBeDisabled();
+  });
+
+  // Add more tests as needed
+});
